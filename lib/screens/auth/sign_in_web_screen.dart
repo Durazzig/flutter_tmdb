9254496666c4a7f7d30697227d484f8d1636e74d
Tadeo Durazo <tadeo_97@outlook.com>
2024-01-03 21:02:40 -0600

    fix: webview authentication fix for android

--- lib/screens/auth/sign_in_web_screen.dart
+++ lib/screens/auth/sign_in_web_screen.dart
@@ -12,7 +12,6 @@ class SignInWebScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final authNotifier = context.read<AuthNotifier>();
-    final router = AppRouter.of(context);
 
     return Scaffold(
       appBar: AppBar(),
@@ -31,142 +30,89 @@ class SignInWebScreen extends StatelessWidget {
               "https://www.themoviedb.org/authenticate/${authNotifier.requestToken}?redirect_to=https://www.flutter-tmdb.com/approved",
             ),
           ),
+          onLoadStart: (controller, url) async {
+            //Android has a weird behavior for redirects, because the webview doesnt "catch" the url thats its being tried to load only after it started loading
+            //So we end up with an error screen for a second until the user is moved back to the home screen.
+            if (context.mounted) {
+              logUser(context, url.toString());
+            }
+          },
           shouldOverrideUrlLoading: (controller, navigationAction) async {
             final url = await controller.getUrl();
-            print("URL:::::::::::");
-            print(url.toString());
-            if (url.toString().contains(
-                "https://www.flutter-tmdb.com/approved?request_token")) {
-              final sessionId = await authNotifier.getSessionID();
-              if (context.mounted) {
-                if (sessionId != null) {
-                  await authNotifier.getProfileInformation();
-                  router.goToScreen(
-                    ChangeNotifierProvider(
-                      create: (context) => HomeScreenNotifier(
-                        hasSessionId: true,
-                      ),
-                      child: const HomeScreen(),
-                    ),
-                  );
-                } else {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text('Sign in error'),
-                        content: const Text(
-                          'There was a problem retrieving your account information, please try again later or continue as a guest.',
-                        ),
-                        actions: <Widget>[
-                          TextButton(
-                            style: TextButton.styleFrom(
-                              textStyle: Theme.of(context).textTheme.labelLarge,
-                            ),
-                            child: const Text('Close'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          TextButton(
-                            style: TextButton.styleFrom(
-                              textStyle: Theme.of(context).textTheme.labelLarge,
-                            ),
-                            child: const Text('Continue as a guest'),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => ChangeNotifierProvider(
-                                    create: (context) => HomeScreenNotifier(
-                                      hasSessionId: false,
-                                    ),
-                                    child: const HomeScreen(),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                }
-              }
+            if (context.mounted) {
+              logUser(context, url.toString());
             }
-            print("hollaaaaa2");
             return NavigationActionPolicy.ALLOW;
           },
           onCreateWindow: (controller, createWindowAction) async {
-            print("create windows");
             return true;
           },
-          // onLoadStart: (controller, url) async {
-          //   if (url.toString().contains(
-          //       "https://www.flutter-tmdb.com/approved?request_token")) {
-          //     final sessionId = await authNotifier.getSessionID();
-          //     if (context.mounted) {
-          //       if (sessionId != null) {
-          //         await authNotifier.getProfileInformation();
-          //         router.goToScreen(
-          //           ChangeNotifierProvider(
-          //             create: (context) => HomeScreenNotifier(
-          //               hasSessionId: true,
-          //             ),
-          //             child: const HomeScreen(),
-          //           ),
-          //         );
-          //       } else {
-          //         showDialog(
-          //           context: context,
-          //           builder: (context) {
-          //             return AlertDialog(
-          //               title: const Text('Sign in error'),
-          //               content: const Text(
-          //                 'There was a problem retrieving your account information, please try again later or continue as a guest.',
-          //               ),
-          //               actions: <Widget>[
-          //                 TextButton(
-          //                   style: TextButton.styleFrom(
-          //                     textStyle: Theme.of(context).textTheme.labelLarge,
-          //                   ),
-          //                   child: const Text('Close'),
-          //                   onPressed: () {
-          //                     Navigator.of(context).pop();
-          //                     Navigator.of(context).pop();
-          //                   },
-          //                 ),
-          //                 TextButton(
-          //                   style: TextButton.styleFrom(
-          //                     textStyle: Theme.of(context).textTheme.labelLarge,
-          //                   ),
-          //                   child: const Text('Continue as a guest'),
-          //                   onPressed: () {
-          //                     Navigator.push(
-          //                       context,
-          //                       MaterialPageRoute(
-          //                         builder: (context) => ChangeNotifierProvider(
-          //                           create: (context) => HomeScreenNotifier(
-          //                             hasSessionId: false,
-          //                           ),
-          //                           child: const HomeScreen(),
-          //                         ),
-          //                       ),
-          //                     );
-          //                   },
-          //                 ),
-          //               ],
-          //             );
-          //           },
-          //         );
-          //       }
-          //     }
-          //   }
-          // },
-          // onLoadError: (controller, url, code, message) async {},
         ),
       ),
     );
+  }
+
+  void logUser(BuildContext context, String url) async {
+    final authNotifier = context.read<AuthNotifier>();
+    final router = AppRouter.of(context);
+    if (url.contains("https://www.flutter-tmdb.com/approved?request_token")) {
+      final sessionId = await authNotifier.getSessionID();
+      if (context.mounted) {
+        if (sessionId != null) {
+          await authNotifier.getProfileInformation();
+          router.goToScreen(
+            ChangeNotifierProvider(
+              create: (context) => HomeScreenNotifier(
+                hasSessionId: true,
+              ),
+              child: const HomeScreen(),
+            ),
+          );
+        } else {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Sign in error'),
+                content: const Text(
+                  'There was a problem retrieving your account information, please try again later or continue as a guest.',
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: Theme.of(context).textTheme.labelLarge,
+                    ),
+                    child: const Text('Close'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: Theme.of(context).textTheme.labelLarge,
+                    ),
+                    child: const Text('Continue as a guest'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChangeNotifierProvider(
+                            create: (context) => HomeScreenNotifier(
+                              hasSessionId: false,
+                            ),
+                            child: const HomeScreen(),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      }
+    }
   }
 }
